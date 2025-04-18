9d0bd346441885dc1dd3be369561f88167ba7a25
Łukasz Małańczuk <l.malanczuk03@gmail.com>
2025-04-19 01:04:45 +0200

    Update AssignDeviceToUser service class: -add validation logic -implement proper error handling -add support for new device creation :)

--- app/services/assign_device_to_user.rb
+++ app/services/assign_device_to_user.rb
@@ -5,14 +5,16 @@ class AssignDeviceToUser
     @requesting_user = requesting_user
     @serial_number = serial_number
     @new_device_owner_id = new_device_owner_id.to_i
-    @device = Device.find_by!(serial_number: @serial_number)
-    @new_owner = User.find(@new_device_owner_id)
+    @device = Device.find_or_initialize_by(serial_number: @serial_number)
+    @new_owner = User.find_by(id: @new_device_owner_id)
+    
+    raise StandardError, "New owner not found" unless @new_owner
   end
 
   def call
     ActiveRecord::Base.transaction do
-            validare_assignment
-            assign_device
+      validate_assignment
+      assign_device
     end
   end
 
@@ -20,26 +22,29 @@ class AssignDeviceToUser
 
   def validate_assignment
     unless @requesting_user.id == @new_device_owner_id
-            raise StandardError, "You can only assign devices to yourself"
+      raise RegistrationError::Unauthorized
     end
 
-    if @device.assigned?
-            raise StandardError, "Device is already assigned to other user"
+    if @device.persisted? && @device.assigned? && @device.user_id != @new_device_owner_id
+      raise AssigningError::AlreadyUsedOnOtherUser
     end
 
-    if @device.previously_assigned_to?(@new_owner)
-            raise StandardError, "You cannot reassign a device you previously returned"
+    if @device.persisted? && @device.previously_assigned_to?(@new_owner)
+      raise AssigningError::AlreadyUsedOnUser
     end
   end
 
   def assign_device
-        assignment = DeviceAssignment.create!(
-          device: @device,
-          user: @new_owner,
-          assigned_at: Time.current
-        )
-        @device.update!(user: @new_owner)
-
-        assignment
+    @device.save! if @device.new_record?
+    
+    assignment = DeviceAssignment.create!(
+      device: @device,
+      user: @new_owner,
+      assigned_at: Time.current
+    )
+    
+    @device.update!(user: @new_owner)
+    
+    assignment
   end
 end
