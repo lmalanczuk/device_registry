944dfa5858ad618db2b78ff75ec9c89e180826c7
Łukasz Małańczuk <l.malanczuk03@gmail.com>
2025-04-19 01:00:06 +0200

    Update User model with device relationships and added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :api_keys, as: :bearer
+  has_many :device_assignments, dependent: :nullify
+  has_many :devices, through: :device_assignments
+  
   has_secure_password
+
+  validates :email, presence: true, uniqueness: true
 end
