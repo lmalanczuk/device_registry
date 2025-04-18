3da043df0aed6b36a508524fb57631b8b9879958
Łukasz Małańczuk <l.malanczuk03@gmail.com>
2025-04-19 01:05:49 +0200

    Update ReturnDeviceFromUser service class: -add validation logic for device return -Implement proper error handling - add support for handling non existent devices

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -1,38 +1,41 @@
 # frozen_string_literal: true
 
 class ReturnDeviceFromUser
-  def initialize(requesting_user:,serial_number:)
-    @requesting_user = requesting_user
+  def initialize(user:, serial_number:, from_user: nil)
+    @requesting_user = user
     @serial_number = serial_number
-    @device = Device.find_by!(serial_number: @serial_number)
+    @from_user_id = from_user || user.id
+    @device = Device.find_by(serial_number: @serial_number)
+
+    raise ReturnError::NotAssigned unless @device
   end
 
   def call
     ActiveRecord::Base.transaction do
-            validate_return
-            return_device
+      validate_return
+      return_device
     end
   end
 
   private
 
   def validate_return
-        unless @device.assigned?
-                    raise StandardError, "Device is not assigned to any user"
-        end
+    unless @device.assigned?
+      raise ReturnError::NotAssigned
+    end
 
-        unless @device.user_id == @requesting_user.id
-                    raise StandardError, "Only the user who has the device assigned can return it"
-        end
+    unless @device.user_id == @from_user_id
+      raise ReturnError::Unauthorized
+    end
   end
 
   def return_device
-        assignment = DeviceAssignment.where(device: @device, user: @requesting_user).active.first
-
-        assignment.return! if assignment
-
-        @device.update!(user: nil)
-
-        assignment
+    assignment = DeviceAssignment.where(device: @device, user_id: @from_user_id).active.first
+    
+    assignment.return! if assignment
+    
+    @device.update!(user: nil)
+    
+    assignment
   end
 end
