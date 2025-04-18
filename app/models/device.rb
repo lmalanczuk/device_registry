b671a50339d15af66dfb210d79694cac66170198
Łukasz Małańczuk <l.malanczuk03@gmail.com>
2025-04-19 01:01:15 +0200

    Update Device model functionality: -add relationship to previous owners -add helper methods for assignment status -improved previously_asigned_to? with persistance check

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,14 +1,22 @@
+# frozen_string_literal: true
+
 class Device < ApplicationRecord
   belongs_to :user, optional: true
   has_many :device_assignments, dependent: :destroy
+  has_many :previous_users, -> { distinct }, through: :device_assignments, source: :user
+  
   validates :serial_number, presence: true, uniqueness: true
-
 
   def assigned?
     user.present?
   end
 
   def previously_assigned_to?(user)
-    device_assignments.where(user: user).exists?
+    return false unless persisted?
+    device_assignments.returned.where(user: user).exists?
+  end
+  
+  def active_assignment
+    device_assignments.active.first
   end
 end
